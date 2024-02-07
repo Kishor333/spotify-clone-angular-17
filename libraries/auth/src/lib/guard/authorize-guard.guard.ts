0621107fbf6747cc3d07f09d43a1ee72729a3012
Kishor Kumar Mongar <kishorkumarmongar@gmail.com>
2024-02-07 20:17:50 +0600

    login started

---
+++ libraries/auth/src/lib/guard/authorize-guard.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+//working
+// let activate = false;
+
+// export const authorizeGuardGuard = (): boolean => {
+//   let router = new Router();
+
+//   if (activate) {
+//           console.log('true');
+//           return true;
+//         } 
+//         else {
+//           console.log('false');
+//           router?.navigate(['login']);
+//           return false;
+//         }
+ 
+//  };
+
+
+@Injectable({ providedIn: 'root' })
+export class authorizeGuardGuard {
+  constructor(
+    public router: Router
+  ) {}
+  activate = false;
+  canActivate(): boolean {
+    if (this.activate) {
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  } 
+}
+  

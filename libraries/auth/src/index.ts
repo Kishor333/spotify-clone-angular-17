6fbfbf51710d0cb173fe6b1cf2df065214adec05
Kishor Kumar Mongar <kishorkumarmongar@gmail.com>
2024-02-07 13:39:58 +0600

    directory structure

---
+++ libraries/auth/src/index.ts
@@ -0,0 +1,5 @@
+import { from } from 'rxjs';
+
+export * from './lib/auth/auth.component';
+export * from './lib/login/login.component';
+
---
+++ libraries/auth/src/lib/auth/auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'spotify-clone-angular-17-auth',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css',
+})
+export class AuthComponent {}

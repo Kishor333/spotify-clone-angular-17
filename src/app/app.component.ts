215361b2bc3b3a96697ad6d1b67f4473fa6446f3
Kishor Kumar Mongar <kishorkumarmongar@gmail.com>
2024-02-20 15:32:02 +0600

    select single album

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent implements OnInit {
       });
     }
     else if (accessToken && accessToken !== 'undefined') {
-      this.router.navigate(['album']);
+      // this.router.navigate(['album']);
       this.getMultipleAlbums();
     }
     else {
